7c6ad09739b5d0c9fede0f40e6aab7a57817367e
Oliver Bredenberg <oliver.bredenberg@dporganizer.com>
2019-04-28 16:56:38 +0200

    React with server boilerplate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import 'dotenv/config';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-console.log('Hello Node.js project.');
+ReactDOM.render(<App />, document.getElementById('root'));
 
-console.log(process.env.MY_SECRET);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
